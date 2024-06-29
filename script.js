a22981c7e8a26324698b3232177e82407a8b4cdd
Marketa Chvaja <meridian.czech@gmail.com>
2024-06-29 15:39:37 +0200

    proměnné + podmínky (věk, váha)

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Hello world!!!");
+// console.log("Hello world!!!");
+
+// let vek = 31;
+
+//let jmeno;
+//jmeno = "Markéta";
+
+// alert("Jmenuji se " + jmeno + " a je mi " + vek + " let.");
+
+
+/*
+// HOW OLD IS USER? 
+let vekUzivatele;
+vekUzivatele = prompt("Zadej svůj věk:");
+
+console.log("Věk uživatele je: " + vekUzivatele);
+
+// ACCESS ONLY FOR USERS 18+
+if (vekUzivatele < 18) {
+    alert("Zákaz vstupu na tento web!")
+} else {
+    console.log("Přístup povolen!")
+};
+
+//WHO IS HEAVIER?
+let vahaAsterix = 55;
+let vahaObelix = prompt("Zadej váhu Obelixe v kilogramech:");
+
+console.log("Váha Asterixe je: " + vahaAsterix);
+console.log("Váha Obelixe je: " + vahaObelix);
+
+if (vahaAsterix > vahaObelix) {
+    console.log ("Asterix je těžší než Obelix.")    
+} 
+else if (vahaObelix > vahaAsterix) {
+    console.log("Obelix je těžší než Asterix.")
+} 
+else {
+    console.log("Oba dva Galové váží stejně.")
+}
+;
+
+*/
+
+// WHO IS OLDER?
+
+
